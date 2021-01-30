db8811de85dd48ebc49e13721f469bf911883c80
Henry Mortimer <henry@morti.net>
2021-01-30 00:21:26 +0000

    feat: add role selecting code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const serve = require('koa-static');
 const mount = require('koa-mount');
 const { createReadStream } = require('fs')
 const PORT = process.env.PORT || 5000
+const GameServer = require('./socket')
 
 const app = new Koa();
 const router = new Router();
@@ -14,10 +15,13 @@ const router = new Router();
 router.get('/', (ctx) => {
   ctx.type = 'html';
   ctx.body = createReadStream('./static/index.html');
+}).get('/reset', (ctx) => {
+  ctx.gameServer.reset()
+  ctx.body = 'reset'
 });
 
 app.use(cors());
-app.use(mount('/static', serve('./static')));
+app.use(mount('/static', serve('../static')));
 app
   .use(router.routes())
   .use(router.allowedMethods());
@@ -27,11 +31,9 @@ const io = new Server(server, {cors: {
   origin: "*",
   methods: ["GET", "POST"]
 }})
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+
+const gs = new GameServer(io)
+app.context.gameServer = gs
+
 
 server.listen(PORT)
