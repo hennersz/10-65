c52b8c149cbdb181429d846d837ebc7243161221
Henry Mortimer <henry@morti.net>
2021-01-30 14:55:38 +0000

    feat: add role classes

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,13 +1,19 @@
-const ROLE_OFFICER = 'ROLE_OFFICER';
-const ROLE_BACK_OFFICE = 'ROLE_BACK_OFFICE';
-const ROLE_WITNESS = 'ROLE_WITNESS';
-const ROLE_DISPATCHER = 'ROLE_DISPATCHER';
-
+const {
+  ROLE_OFFICER,
+  ROLE_DISPATCHER,
+  ROLE_BACK_OFFICE, 
+  ROLE_WITNESS, 
+  IN_GAME,
+  IN_LOBBY,
+  UNMATCHED
+} = require('../client/src/constants')
 const ROLES = [ROLE_OFFICER, ROLE_DISPATCHER, ROLE_WITNESS, ROLE_BACK_OFFICE]
+const EventEmitter = require('events')
 
-class GameServer {
+class GameServer extends EventEmitter {
   constructor(io) {
-    this.io= io;
+    super()
+    this.io = io
     this.availableRoles = [...ROLES]
     this.takenRoles = {}
     io.on('connection', this.connect.bind(this))
@@ -17,21 +23,96 @@ class GameServer {
     console.log('connected')
     const role = this.availableRoles.pop()
     if(role) {
-      this.takenRoles[socket.id] = role
-      socket.emit('game_state_change', {state: 'IN_LOBBY', role: role})
+      this.assignRole(socket, role)
     } else {
-      socket.emit('game_state_change', {state: 'UNMATCHED', msg: 'no roles available'})
+      console.log('no roles available')
+      socket.emit('game_state_change', {state: UNMATCHED, msg: 'no roles available'})
     }
   }
 
+  assignRole(socket, role) {
+    console.log('assigning role')
+    let roleObj
+    switch (role) {
+      case ROLE_DISPATCHER:
+        roleObj = new Dispatcher(this, socket)    
+        this.takenRoles[socket.id] = roleObj
+        break;
+      case ROLE_OFFICER:
+        roleObj = new Officer(this, socket)    
+        this.takenRoles[socket.id] = roleObj
+        break;
+      case ROLE_WITNESS:
+        roleObj = new Witness(this, socket)    
+        this.takenRoles[socket.id] = roleObj
+        break;
+      case ROLE_BACK_OFFICE:
+        roleObj = new BackOffice(this, socket)    
+        this.takenRoles[socket.id] = roleObj
+        break;
+    }
+    const disconnectFn = this.disconnect.bind(this)
+    socket.on('disconnect', ()=>{
+      disconnectFn(socket)
+    })
+    socket.emit('game_state_change', {state: IN_LOBBY, role: role})
+  }
+
   disconnect(socket) {
-    this.availableRoles.push(this.takenRoles[socket.id])
+    console.log('disconnected')
+    this.availableRoles.push(this.takenRoles[socket.id].name)
     delete this.takenRoles[socket.id]
+  }
+
+  start(){
+    this.io.sockets.emit('game_state_change', {state: IN_GAME})
   }
 
   reset() {
     console.log('resetting')
     this.availableRoles = [...ROLES]
+  }
+}
+
+class Role {
+  constructor(gs, socket, name) {
+    this.gameServer = gs
+    this.socket = socket
+    this.name = name
+  }
+}
+
+class Officer extends Role{
+  constructor(gs, socket) {
+    super(gs, socket, ROLE_OFFICER)
+    this.gameServer.on('move_event', this.handleMove.bind(this))
+  }
+
+  handleMove(location) {
+    this.socket.emit('move', location)
+  }
+}
+
+class Dispatcher extends Role{
+  constructor(gs, socket) {
+    super(gs, socket, ROLE_DISPATCHER)
+
+    this.socket.on('move', this.handleMove.bind(this))
+  }
+  handleMove(location) {
+    this.gameServer.emit('move_event', location)
+  }
+}
+
+class Witness extends Role{
+  constructor(gs, socket) {
+    super(gs, socket, ROLE_WITNESS)
+  }
+}
+
+class BackOffice extends Role {
+  constructor(gs, socket) {
+    super(gs, socket, ROLE_BACK_OFFICE)
   }
 }
 
