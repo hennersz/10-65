c52c76e07d5d458aaf3aaae40533963bcf2fe9bd
Joshua Allwood <jjallwood@live.co.uk>
2021-01-31 13:36:15 +0000

    feat(Witness): delayed clue giving

--- src/client/src/Components/Witness.jsx
+++ src/client/src/Components/Witness.jsx
@@ -1,12 +1,14 @@
 import data from '../data/imogen.json'
 import React from "react";
 
+const CLUE_DELAY = 3;
+
 const Witness = ({minutes}) => {
     return (
         <div>
             {
                 data.witness.map(({ clue }, index) => {
-                    if (index <= (minutes + 1)) {
+                    if (index * CLUE_DELAY <= (minutes + CLUE_DELAY)) {
                         return (
                             <div className="card">
                                 <div className="card-body">
@@ -18,7 +20,7 @@ const Witness = ({minutes}) => {
                     return (
                         <div className="card">
                             <div className="card-body">
-                                <h5 className="card-title">{"This will be revealed in " + (index - (minutes + 1)) + " minutes" }</h5>
+                                <h5 className="card-title">{"This will be revealed in " + (index * CLUE_DELAY - (minutes + CLUE_DELAY)) + " minutes" }</h5>
                             </div>
                         </div>
                     )
